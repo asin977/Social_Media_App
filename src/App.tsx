6207549aa380f38001dbebda37fefb9b07cdd086
asin977 <assinannameckal@gmail.com>
2025-07-09 16:36:06 +0530

    feat:Implemented header on the homePage (#1)
    * feat:Implemented header on the homePage
    
    * refractor:resolved the review comments
    
    * refractor:resolved review comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { routes } from './routes';
+import { Home } from './pages';
 
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path={routes.home} element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import UserIcon from '../assets/images/userIcon.png';
+
+export const Header = () => (
+  <div
+    style={{
+      padding: '1.5rem',
+      background: '#62b6cb',
+      display: 'flex',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      cursor: 'pointer',
+    }}
+  >
+    <img
+      src={UserIcon}
+      alt="User Icon"
+      style={{ height: '50px', marginRight: '12px' }}
+    />
+  </div>
+);
