be073bd9d144bf8ac039e0e7ab5f195924269d98
Assin Anna <assinannameckal@gmail.com>
2025-07-09 20:00:17 +0530

    rendered the userdetails api on the homePage

--- src/hooks/FetchUsers.tsx
+++ src/hooks/FetchUsers.tsx
@@ -2,17 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 import { User } from '../types/user';
 
 const fetchUsers = async (): Promise<User[]> => {
-  const res = await fetch('https://gorest.co.in/public/v2/users'); 
+  const res = await fetch('https://gorest.co.in/public/v2/users');
   if (!res.ok) throw new Error('Failed to fetch users');
   return res.json();
 };
 
-const UserList = () => {
-  const { data, isLoading, isError, error } = useQuery<User[], Error>({
+export const useUsers = () =>
+  useQuery<User[], Error>({
     queryKey: ['users'],
     queryFn: fetchUsers,
   });
-
-  if (isLoading) return <p>Loading users...</p>;
-  if (isError) return <p>Error: {error.message}</p>;
-}
