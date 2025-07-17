55ccc91d9dbc05d3ba94a0970a92a7a0cf389b49
Assin Anna <assinannameckal@gmail.com>
2025-07-17 20:32:35 +0530

    feat:updated the UI of the usersPost page

--- src/pages/GetUsersPost.tsx
+++ src/pages/GetUsersPost.tsx
@@ -1,5 +1,6 @@
 import { useGetUserPosts } from '../apis/user';
 import { Header } from '../components/Header';
+import UserIcon from '../assets/images/userIcon.png';
 
 export const GetUsersPost = () => {
   const { data: posts, isLoading, isError, error } = useGetUserPosts();
@@ -12,16 +13,48 @@ export const GetUsersPost = () => {
     return <p>Error:{error?.message}</p>;
   }
   return (
-    <div>
+    <>
       <Header />
-      <h2 style={{color:'darkblue',fontSize:'30px'}}>USER POST</h2>
-      {posts?.map(post => (
-        <div>
-          <h2 style={{color:'darkblue'}}>{post.title}</h2>
-          <p>{post.body}</p>
-          <p>{post.user_id}</p>
-        </div>
-      ))}
-    </div>
+      <h2 style={{ color: 'darkblue', fontSize: '30px' }}>USER POST</h2>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(3,1fr)',
+          justifyItems: 'start',
+          textAlign: 'justify',
+          marginLeft: '100px',
+          marginRight: '30px',
+          gap: '30px',
+          marginTop: '50px',
+          marginBottom: '50px',
+        }}
+      >
+        {posts?.map(post => (
+          <div
+            style={{
+              color: 'black',
+              backgroundColor: '#e3f2fd',
+              boxShadow: '1px 2px 3px blue',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+              flexDirection: 'column',
+              width: '100%',
+              padding: '20px',
+              fontFamily: 'bold',
+              fontSize: '20px',
+            }}
+          >
+            <span>
+              <img style={{ width: '60px' }} src={UserIcon} />
+            </span>
+            <h2 style={{ color: 'darkblue', display: 'grid' }}>{post.title}</h2>
+            <p>{post.body}</p>
+            <p style={{ color: 'darkred' }}>{post.user_id}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
