5f81dfe65f1c310044856e60195eab2b14b83cd2
Assin Anna <assinannameckal@gmail.com>
2025-07-17 17:49:46 +0530

    refractor:update the code

--- src/apis/user/useAddUser.ts
+++ src/apis/user/useAddUser.ts
@@ -3,16 +3,16 @@ import { UserListAPIResponse } from '../../types/user';
 import { endpoints } from '../endpoints';
 
 export const addUser = async (payload: Partial<UserListAPIResponse>) => {
-  const response = await httpClient.post(endpoints.createUser(),payload);
+  const response = await httpClient.post(endpoints.createUser(), payload);
 
   if (!response || response.status !== 201) {
     const data = response.data;
     throw new Error(
       Array.isArray(data) && data.length > 0
         ? data[0].message
-        : data?.message || 'Failed to add user'
+        : data?.message || 'Failed to add user',
     );
   }
 
   return response.data;
-}
+};
