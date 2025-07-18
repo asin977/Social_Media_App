d54a059b7602d7e753527a41de17c9e9324578bb
vonnue-assin <assin.anna@vonnue.com>
2025-07-18 10:10:22 +0000

    refrcator:implemented the code formtating

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { routes } from './routes';
 import { Home } from './pages';
+import { routes } from './routes';
 
 const queryClient = new QueryClient();
 
