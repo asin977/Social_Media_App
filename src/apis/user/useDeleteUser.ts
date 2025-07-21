d1af04c92d81e550408ee19330fd2d4e3cd1a0a4
vonnue-assin <assin.anna@vonnue.com>
2025-07-21 09:51:00 +0000

    refractor:resolved the review comments

--- src/apis/user/useDeleteUser.ts
+++ src/apis/user/useDeleteUser.ts
@@ -1,28 +1,37 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
 
 import { DataQueryKeys } from '../data-query-keys';
 import httpClient from '../httpClient';
 import { endpoints } from '../endpoints';
 
-const deleteUserRequest = async (userId: string) => {
-
-
-  const response = await httpClient.delete<void>(endpoints.deleteUser(userId));
-
-  return response.data;
-};
-
 export const useDeleteUser = () => {
   const queryClient = useQueryClient();
 
   return useMutation<void, Error, string>({
-    mutationFn: deleteUserRequest,
+    mutationFn: async (userId: string) => {
+      const response = await httpClient.delete<void>(
+        endpoints.deleteUser(userId),
+      );
+
+      if (!response.config) {
+        throw new Error('Failed to delete user.');
+      }
+      return response.data;
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [DataQueryKeys.USER_LIST] });
-      alert('User deleted successfully...');
+      toast.success('User deleted successfully!', {
+        position: 'top-right',
+        autoClose: 2000,
+      });
     },
     onError: error => {
-      alert('Error deleting User,');
+      console.error('Error deleting user:', error);
+      toast.error(`Error deleting user: ${error.message || 'Unknown error'}`, {
+        position: 'top-right',
+        autoClose: 3000,
+      });
     },
   });
 };
