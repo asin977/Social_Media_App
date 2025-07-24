2160fbf9997a92522e0a9a074a33bd853bfc9ad2
vonnue-assin <assin.anna@vonnue.com>
2025-07-24 10:24:20 +0530

    refractor:resolved the review comments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Header } from '../components/Header';
-import GetUserListItem from '../components/GetUsersList';
+import GetUserListItem from '../components/UserList';
 
 export const Home = () => (
   <>
