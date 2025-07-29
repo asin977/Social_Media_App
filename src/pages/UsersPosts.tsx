deb407dc6bc95fac8f9ba61253627eac353772a6
Assin Anna <assinannameckal@gmail.com>
2025-07-29 18:22:33 +0530

    refactor:resolved the review comments

--- src/pages/UsersPosts.tsx
+++ src/pages/UsersPosts.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-
-import { Header } from '../components/Header';
-import {CommentList} from '../components/CommentList';
+import { CommentList } from '../components/CommentList';
 
 export const UserPosts = () => (
   <>
-    <Header />
     <CommentList />
   </>
 );
